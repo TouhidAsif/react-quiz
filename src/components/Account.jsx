cdadbd43bff7080b3977d1128e13f73d1f2c10e7
Touhidur Rahaman <touhid@squarehospital.com>
2022-06-11 14:06:59 +0600

    react-quiz-code ls4

---
+++ src/components/Account.jsx
@@ -0,0 +1,12 @@
+import classes from "../styles/Layout.module.css"
+export default function Account(){
+    return (
+        <div className={classes.account}>
+        <span class="material-icons-outlined" title="Account">
+          account_circle
+        </span>
+        <a href="signup.html">Signup</a>
+         {/* <span class="material-icons-outlined" title="Logout"> logout </span>  */}
+      </div>
+    )
+  }
---
+++ src/components/Videos.jsx
@@ -0,0 +1,9 @@
+import classes from "../styles/Videos.module.css"
+import Video from "./Video"
+export default function Videos(){
+    return(
+        <div className={classes.videos}>
+            <Video/>
+        </div>
+    )
+}
